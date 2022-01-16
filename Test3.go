40b35414d93285b2a421e31739785a5dbdbea035
Wali Raza <walimanzoor321@gmail.com>
2022-01-16 20:40:20 +0500

    Type_COnversion(int-float32)

--- Test3.go
+++ Test3.go
@@ -1,12 +1,18 @@
 package main
 
-//type conversion
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	a := 5
-	fmt.Printf("%T,%v", a, a, "/n")
+	fmt.Printf("%T %v ", a, a)
+	fmt.Println()
 	var b float32 = float32(a)
 	fmt.Println()
-	fmt.Printf("%T  , %v", b, "/n", b)
+	fmt.Printf("%T   %v", b, b)
+	var c string = strconv.Itoa(a)
+	fmt.Println()
+	fmt.Printf(c)
 }
